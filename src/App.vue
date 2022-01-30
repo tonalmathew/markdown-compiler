063150682cf572be6729c951910abb7236b0e332
tonalmathew <tonalmathew1999@gmail.com>
2022-01-30 22:39:46 +0530

    chore: ui change and disable extensions

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <dark-mode />
-  <div class="grid grid-cols-2 grid-flow-col gap-4">
-    <div>
+  <div class="flex flex-col lg:flex-row gap-4 h-screen m-4">
+    <div class="grow w-full">
       <textarea
-        class="text-gray-900 dark:text-white dark:bg-gray-700"
+        class="text-gray-900 dark:text-white dark:bg-gray-700 rounded w-full h-4/5 focus:outline-none focus:border-none resize-none "
         v-model="markdown"
         rows="20"
+        data-gramm_editor="false"
+        
       ></textarea>
     </div>
-    <div>
-      <div v-html="compileIt" class="text-gray-900 dark:text-white"></div>
+    <hr class="w-full lg:w-0 lg:h-4/5 m-1"/>
+    <div class="grow w-full h-full">
+      <div v-html="compileIt" class="text-gray-900 dark:text-white dark:bg-gray-700 p-1 overflow-y-auto rounded h-4/5"></div>
     </div>
   </div>
 </template>
@@ -38,13 +41,13 @@ export default {
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
 
 * {
   font-family: "Rubik", sans-serif;
 }
-textarea {
+/* textarea {
   resize: none !important;
   width: 100%;
-}
+} */
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'Rubik': ['"Rubik"', 'sans-serif'] // Ensure fonts with spaces have " " surrounding it.
+    },
     extend: {},
   },
   variants: {
