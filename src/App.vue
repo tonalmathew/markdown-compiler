24985b1999bd05aa82583a1dc1da540bac571ce9
tonalmathew <tonalmathew1999@gmail.com>
2022-01-30 19:21:43 +0530

    chore: font to rubic

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <dark-mode />
-  <!-- <div class="bg-white dark:bg-gray-800">
-    <h1 class="text-gray-900 dark:text-white">Dark mode</h1>
-    <p class="text-gray-600 dark:text-gray-300">Lorem ipsum...</p>
-  </div> -->
   <div class="grid grid-cols-2 grid-flow-col gap-4">
-  <div>
-    <textarea class="text-gray-900 dark:text-white dark:bg-gray-700" v-model="markdown" rows="20"></textarea>
+    <div>
+      <textarea
+        class="text-gray-900 dark:text-white dark:bg-gray-700"
+        v-model="markdown"
+        rows="20"
+      ></textarea>
+    </div>
+    <div>
+      <div v-html="compileIt" class="text-gray-900 dark:text-white"></div>
+    </div>
   </div>
-  <div><div v-html="compileIt" class="text-gray-900 dark:text-white"></div></div>
-</div>
 </template>
 
 <script>
@@ -36,6 +38,11 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
+* {
+  font-family: "Rubik", sans-serif;
+}
 textarea {
   resize: none !important;
   width: 100%;
