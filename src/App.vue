76578662872e54fc0265a39da2c41592c6822acb
tonalmathew <tonalmathew1999@gmail.com>
2022-01-30 23:10:10 +0530

    chore:font large

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <dark-mode />
-  <div class="flex flex-col lg:flex-row gap-4 h-screen m-4">
+  <div class="flex flex-col lg:flex-row gap-4 h-screen m-4 mt-8">
     <div class="grow w-full">
       <textarea
-        class="text-gray-900 dark:text-white dark:bg-gray-700 rounded w-full h-4/5 focus:outline-none focus:border-none resize-none "
+        class="text-gray-900 bg-gray-200 dark:text-white dark:bg-gray-700 rounded w-full h-4/5 focus:outline-none focus:border-none resize-none text-base"
         v-model="markdown"
         rows="20"
         data-gramm_editor="false"
-        
       ></textarea>
     </div>
-    <hr class="w-full lg:w-0 lg:h-4/5 m-1"/>
+    <hr
+      class="w-full lg:w-0 lg:h-4/5 m-1 border-black bg-black dark:border-white dark:bg-white"
+    />
     <div class="grow w-full h-full">
-      <div v-html="compileIt" class="text-gray-900 dark:text-white dark:bg-gray-700 p-1 overflow-y-auto rounded h-4/5"></div>
+      <div
+        v-html="compileIt"
+        class="text-gray-900 bg-gray-200 dark:text-white dark:bg-gray-700 p-1 overflow-y-auto rounded h-4/5 text-base"
+      ></div>
     </div>
   </div>
 </template>
@@ -41,13 +45,7 @@ export default {
 </script>
 
 <style>
-
-
 * {
   font-family: "Rubik", sans-serif;
 }
-/* textarea {
-  resize: none !important;
-  width: 100%;
-} */
 </style>
